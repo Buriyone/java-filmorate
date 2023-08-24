5f03b092d507bf5765ff20ee7affe03172c3d772
Buriyone <122684439+Buriyone@users.noreply.github.com>
2023-08-24 04:18:02 +0300

    refactor for checkstyle again

--- src/test/java/develop/FilmorateApplicationTests.java
+++ src/test/java/develop/FilmorateApplicationTests.java
@@ -2,7 +2,6 @@ package develop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
